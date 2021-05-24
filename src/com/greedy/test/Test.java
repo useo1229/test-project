be0defed651ad1bc0e064b287845289edacf87e5
tj2 <tj2@DESKTOP-64T8B5K>
2021-05-24 12:30:00 +0900

    숫자 출력 삭제

--- src/com/greedy/test/Test.java
+++ src/com/greedy/test/Test.java
@@ -5,9 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("박윤서");
-		System.out.println(123);
-		System.out.println(456);
-		System.out.println(789);
+		
 		
 	}
 
